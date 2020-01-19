1764b06746a1389df6371320bb32be1dc4f7bc41
layerism <lcyqky@icloud.com>
2020-01-19 10:10:12 +0800

    fix bug

--- example/detection/client.sh
+++ example/detection/client.sh
@@ -4,5 +4,5 @@ IMAGE="../test-data/widerface.jpg"
 python client.py \
     --model-name detection \
     --model-version 1 \
-    --url "10.160.168.155:7001" \
+    --url "x.x.x.x:7001" \
     --image ${IMAGE}
